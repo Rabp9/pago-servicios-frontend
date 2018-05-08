f808fa3818ae313ae3170ea19783447496835f85
tmtrbocanegra <rabp_91@hotmail.com>
2018-05-08 11:44:47 -0500

    30. Reporte de Servicios - controller

--- app/scripts/controllers/reporte-servicios.js
+++ app/scripts/controllers/reporte-servicios.js
@@ -8,34 +8,33 @@
  * Controller of the pagoServiciosFrontendApp
  */
 angular.module('pagoServiciosFrontendApp')
-.controller('ReporteServiciosCtrl', function ($scope, serviciosservice, tiposservice) {
-    function formatDate(fecha) {
-        if (fecha === undefined) {
-            return undefined;
-        }
-        return fecha.getFullYear() + '-' + str_pad((fecha.getMonth() + 1), '00') + '-' + str_pad(fecha.getDate(), '00');
-    }
-    
-    function str_pad(str, pad) {
-        return pad.substring(0, (pad.length - str.toString().length)) + str;
-    }
-    
+.controller('ReporteServiciosCtrl', function ($scope, serviciosservice, tiposservice, $utilsViewService) {
     $scope.search = {};
-    $scope.search.condicion = '';
-    $scope.loading = false;
-    var today = new Date();
-    $scope.today = formatDate(today);
-    
+    $scope.items_per_page = 10;
+    $scope.page = 1;
+    $scope.tipos_ws = {
+       wCodigo: '5%',
+       wDescripcion: '32%',
+       wDetalle: '14%',
+       wNRecibos: '14%',
+       wNRecibosSinPagar: '14%',
+       wMontoPendiente: '21%',
+    };
+    $scope.labelsBar = [];
+    $scope.seriesBar = ['Monto Total Pendiente de Pago'];
+    $scope.optionsBar = {legend: {display: true}};
+    $scope.dataBar = [];
+   
     $scope.init = function() {
-        $scope.loading = true;
+        var date = new Date();
+        var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+        var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+        $scope.search.fechaInicio = firstDay;
+        $scope.search.fechaCierre = lastDay;
         $scope.loading_tipos = 'Cargando...';
         tiposservice.get(function(data) {
             $scope.loading_tipos = 'Selecciona un Tipo';
             $scope.tipos = data.tipos;
-            serviciosservice.getReport({tipo_id: 0}, function(data) {
-                $scope.servicios = data.servicios;
-                $scope.loading = false;
-            });
         });
     };
     
@@ -43,12 +42,38 @@ angular.module('pagoServiciosFrontendApp')
         if (tipo_id === undefined) {
             tipo_id = 0;
         }
+        $scope.getReporteServicios(tipo_id);
+    };
+    
+    $scope.getReporteServicios = function(tipo_id) {
         $scope.loading = true;
-        serviciosservice.getReport({tipo_id: tipo_id}, function(data) {
-            $scope.loading = false;
-            $scope.loading_servicios = 'Seleccione un servicio';
+        var fechaInicio = $utilsViewService.formatDate($scope.search.fechaInicio);
+        var fechaCierre = $utilsViewService.formatDate($scope.search.fechaCierre);
+        serviciosservice.getReport({
+            tipo_id: tipo_id,
+            fechaInicio: fechaInicio,
+            fechaCierre: fechaCierre,
+            page: $scope.page,
+            items_per_page: $scope.items_per_page
+        }, function(data) {
             $scope.servicios = data.servicios;
+            var montoPendienteArray = [];
+            angular.forEach($scope.servicios, function(val, key) {
+                $scope.labelsBar.push(val.descripcion);
+                montoPendienteArray.push(val.montoPendiente);
+            });
+            $scope.dataBar.push(montoPendienteArray);
+            $scope.loading = false;
         });
+    };
+    
+    $scope.onChangeItemsPerPage = function(tipo_id) {
+        $scope.page = 1;
+        $scope.getReporteServicios(tipo_id);
+    };
+    
+    $scope.pageChanged = function(tipo_id) {
+        $scope.getReporteServicios(tipo_id);
     };
     
     $scope.exportData = function (option) {
